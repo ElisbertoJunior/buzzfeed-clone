f2dbec1e78ac9a482a753f86fba8d9583043890e
Elisberto Junior <elisberto_jr@hotmail.com>
2022-11-23 20:55:46 -0300

    add answers

--- src/app/componets/quizz/quizz.component.ts
+++ src/app/componets/quizz/quizz.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import quizz_questions from 'src/assets/data/quizz_questions.json'
+
 
 @Component({
   selector: 'app-quizz',
@@ -22,7 +24,32 @@ export class QuizzComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    
+    if (quizz_questions) {
+      this.finished = false
+      this.title = quizz_questions.title
+
+      this.questions = quizz_questions.questions
+      this.questionSelected = this.questions[this.questionIndex]
+
+      this.questionMaxIndex = this.questions.length
+    }
+  }
+
+  playerChoose(value: string) {
+    this.answers.push(value)
+    this.nextStep()
+
+  }
+
+  nextStep () {
+    this.questionIndex += 1
+
+    if (this.questionMaxIndex > this.questionIndex) {
+      this.questionSelected = this.questions[this.questionIndex]
+    }
+
+    this.finished = true
+
   }
 
 }
