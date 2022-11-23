36a214f3722f482b7968823d05539e2b635b616d
Elisberto Junior <elisberto_jr@hotmail.com>
2022-11-22 22:11:26 -0300

    add components

--- src/app/componets/quizz/quizz.component.ts
+++ src/app/componets/quizz/quizz.component.ts
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quizz.component.css']
 })
 export class QuizzComponent implements OnInit {
+  title: string = ""
+  questions: any = ""
+  questionSelected: any 
 
   constructor() { }
 
   ngOnInit(): void {
+
   }
 
 }
