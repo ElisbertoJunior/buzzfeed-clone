821e862ef22d4ade254939d48dbb0a949c2bef8a
Elisberto Junior <elisberto_jr@hotmail.com>
2022-11-23 21:30:04 -0300

    add functions

--- src/app/componets/quizz/quizz.component.ts
+++ src/app/componets/quizz/quizz.component.ts
@@ -41,15 +41,41 @@ export class QuizzComponent implements OnInit {
 
   }
 
-  nextStep () {
+  async nextStep () {
     this.questionIndex += 1
 
     if (this.questionMaxIndex > this.questionIndex) {
       this.questionSelected = this.questions[this.questionIndex]
     }
 
+    else {
+
+    const finalAnswer: string = await this.checkResult(this.answers)
+
     this.finished = true
+    this.answerSelected = quizz_questions.results[finalAnswer as keyof
+    typeof quizz_questions.results]
+
+    }
 
   }
+
+ async checkResult (answers: string[]) {
+  const result = answers.reduce((previous, current, i, arr) => {
+    if (
+      arr.filter(item => item === previous).length >
+      arr.filter(item => item === current).length
+      ) {
+        return previous
+    }
+
+    return current
+  })
+
+  return result
+
+ }
+
+
 
 }
